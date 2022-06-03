c91b8c7bc582eaace8d1770ecc9867575fbfa53b
long.truong <long.truong@linecorp.com>
2022-06-03 10:53:40 +0700

    fix strict issue 02

--- js/mobile.js
+++ js/mobile.js
@@ -1,43 +1,43 @@
 /*------ -control- ------*/
 function loopToggleMobile() {
-	if (media.loop) {
-		startLoopAllMobile();     
-	} else if (isLoopAll) {
-    stopLoopAllMobile()
-  } else {    
-		startLoopMobile();
-	}
+  if (media.loop) {
+    startLoopAllMobile();
+  } else if (isLoopAll) {
+    stopLoopAllMobile();
+  } else {
+    startLoopMobile();
+  }
 }
 
 function startLoopAllMobile() {
-  media.loop = false;    
+  media.loop = false;
   isLoopAll = true;
-  getElement("loop-button-tooltip-mobile").textContent = "Stop loop all";  
-  domElement.loopToggleButtonMobile.firstChild.classList.remove('fa-redo-alt')    
-  domElement.loopToggleButtonMobile.firstChild.classList.add('fa-retweet') 
+  getElement('loop-button-tooltip-mobile').textContent = 'Stop loop all';
+  domElement.loopToggleButtonMobile.firstChild.classList.remove('fa-redo-alt');
+  domElement.loopToggleButtonMobile.firstChild.classList.add('fa-retweet');
 }
 
-function stopLoopAllMobile() {    
-  isLoopAll = false; 
-  getElement("loop-button-tooltip-mobile").textContent = "Start loop"; 
-  domElement.loopToggleButtonMobile.firstChild.classList.remove('fa-retweet')    
-  domElement.loopToggleButtonMobile.firstChild.classList.add('fa-redo-alt')     
-  domElement.loopToggleButtonMobile.classList.add("text--gray");
+function stopLoopAllMobile() {
+  isLoopAll = false;
+  getElement('loop-button-tooltip-mobile').textContent = 'Start loop';
+  domElement.loopToggleButtonMobile.firstChild.classList.remove('fa-retweet');
+  domElement.loopToggleButtonMobile.firstChild.classList.add('fa-redo-alt');
+  domElement.loopToggleButtonMobile.classList.add('text--gray');
 }
 
-function startLoopMobile() {    
-  media.loop = true;    
-  getElement("loop-button-tooltip-mobile").textContent = "Start loop all"; 
-  domElement.loopToggleButtonMobile.classList.remove("text--gray");   
+function startLoopMobile() {
+  media.loop = true;
+  getElement('loop-button-tooltip-mobile').textContent = 'Start loop all';
+  domElement.loopToggleButtonMobile.classList.remove('text--gray');
 }
 
 function showListToggleMobile() {
-  var songListPanel = getElement("song-list-panel");
-  if (isListShow) {
-    isListShow = false;
-    songListPanel.style.right = "-120vw";
+  var songListPanel = getElement('song-list-panel');
+  if (appSetting.isListShow) {
+    appSetting.isListShow = false;
+    songListPanel.style.right = '-120vw';
   } else {
-    isListShow = true;
-    songListPanel.style.right = "0";
+    appSetting.isListShow = true;
+    songListPanel.style.right = 0;
   }
 }
